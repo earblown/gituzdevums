f4f8d0ba5a95837961bee1a0ebc96d88b2556d63
earblown <giiiaaat@gmail.com>
2019-05-14 10:24:44 +0300

    pirmais comm

---
+++ js.js
@@ -0,0 +1,44 @@
+function task (atzime){
+if(atzime == 1)
+{
+  console.log(atzime+"💩")
+}
+else if(atzime == 2)
+{
+  console.log(atzime+"😕")
+}
+else if(atzime == 3)
+{
+  console.log(atzime+"😂")
+}
+else if(atzime == 4)
+{
+  console.log(atzime+"👌")
+}
+else if(atzime == 5)
+{
+  console.log(atzime+"🤙🏻")
+}
+else if(atzime == 6)
+{
+  console.log(atzime+"😯")
+}
+else if(atzime == 7)
+{
+  console.log(atzime+"🤔")
+}
+else if(atzime == 8)
+{
+  console.log(atzime+"💪🏻")
+}
+else if(atzime == 9)
+{
+  console.log(atzime+"😏")
+}
+else if(atzime == 10)
+{
+  console.log(atzime+"🖕🏻")
+}
+}
+console.log(task(6));
+console.log("kjhkj");
